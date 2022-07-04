c4bd31955b94e8709f718d1f90ad7d68e7057745
Patrick Sanzo <patrick.sanzo@ltnglobal.com>
2022-07-04 14:32:08 +0200

    chapter 4: Lazy sequences

--- code/chapter4-functional-programming.clj
+++ code/chapter4-functional-programming.clj
@@ -38,3 +38,15 @@
               current
               (recur next (+ current next) (dec n)))]
     (fib 0N 1N n)))
+
+
+;  ------------------------ lazy sequences
+(defn lazy-seq-fibo
+  ([]
+    (concat [0 1] (lazy-seq-fibo 0N 1N)))
+  ([a b]
+    (let [n (+ a b)]
+      (lazy-seq (cons n (lazy-seq-fibo b n))))))
+
+(defn fibo []
+  (map first (iterate (fn [[a b]] [b (+ a b)]) [0N 1N])))
