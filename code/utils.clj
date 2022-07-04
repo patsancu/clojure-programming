c2ecaa09d549c51ef4ed186a874b4f727278651b
Patrick Sanzo <patrick.sanzo@ltnglobal.com>
2022-07-04 17:58:57 +0200

    misc: Limit repl printing to 10 elements

--- code/utils.clj
+++ code/utils.clj
@@ -5,3 +5,10 @@
       end (System/nanoTime)]
    (println (str "Elapsed time: " (- end start) " ns"))
    ret)
+
+
+; limit repl printing to 10 elements
+(set! *print-length* 10
+; 10
+(iterate inc 1)
+; (1 2 3 4 5 6 7 8 9 10 ...)
