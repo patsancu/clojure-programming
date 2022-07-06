d355ca2ac84b3a0332abec71b66c2d14bf653841
Patrick Sanzo <patrick.sanzo@ltnglobal.com>
2022-07-06 15:57:25 +0200

    utils: Check nicely how much time a function requires
    REPL config for nested stuff

--- code/utils.clj
+++ code/utils.clj
@@ -1,10 +1,11 @@
 
+; BULLSHIT, just use (time (f value))
 ; Find out how much time a function requires in ns
-(let [start (System/nanoTime)
-    ret (stack-consuming-fibonacci 40)
-      end (System/nanoTime)]
-   (println (str "Elapsed time: " (- end start) " ns"))
-   ret)
+; (let [start (System/nanoTime)
+    ; ret (stack-consuming-fibonacci 40)
+      ; end (System/nanoTime)]
+   ; (println (str "Elapsed time: " (- end start) " ns"))
+   ; ret)
 
 
 ; limit repl printing to 10 elements
@@ -12,3 +13,19 @@
 ; 10
 (iterate inc 1)
 ; (1 2 3 4 5 6 7 8 9 10 ...)
+
+(deeply-nested 100)
+; (((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((((bottom)))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))
+(set! *print-level* 25)
+(defn deeply-nested [n]
+  (loop [n n
+          result '(bottom)]
+    (if (= n 0)
+      result
+      (recur (dec n) (list result)))))
+(set! *print-level* 25)
+; 25
+(deeply-nested 5)
+; ((((((bottom))))))
+(deeply-nested 100)
+; (((((((((((((((((((((((((#)))))))))))))))))))))))))
